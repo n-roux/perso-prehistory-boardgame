d4f6dd311583dcccdbacc9c1c0fabb8b7437d2ec
n-roux <nrouxnicolas@gmail.com>
2018-05-02 11:26:31 +0200

    Added gui method prototypes

--- src/gui.py
+++ src/gui.py
@@ -6,19 +6,79 @@ Created on 26 avr. 2018
 from Tkinter import *
 
 
-'''
-
-'''
-
-class game_GUI(object):
+class SoloBetatestGUI(Frame):
     '''
-    GUI is not started yet.
+    A GUI designed for a single beta-tester:
+    One player can play the 5 classes of the game.
     
+    This Frame is the master GUI and handles all the other Frame classes below (see GUI draft on github or GDrive)
     '''
+    def __init__(self, *args, **kwargs):
+        Frame.__init__(self,*args, **kwargs)
+        #self.configure(cnf)
+        raise NotImplementedError
 
+class WorldMapFrame(Frame):
+    '''
+    Handles the representation of the revealed board tiles
+    '''
+    def __init__(self, *args, **kwargs):
+        Frame.__init__(self,*args, **kwargs)
 
-    def __init__(self, params):
-        '''
-        Constructor
-        '''
+class PassiveTribeFrame(Frame):
+    '''
+    Handles the representation of the passive tribe status and food needs
+    '''
+    def __init__(self, *args, **kwargs):
+        Frame.__init__(self,*args, **kwargs)
+
+class ActivePlayerFrame(Frame):
+    '''
+    Handles the representation of the active player representation (maybe not useful in solo beta-testing) 
+    '''
+    def __init__(self, *args, **kwargs):
+        Frame.__init__(self,*args, **kwargs)
         
+class TurnCounterFrame(Frame):
+    '''
+    Handles the representation of the current turn count, and the end turn button
+    '''
+    def __init__(self, *args, **kwargs):
+        Frame.__init__(self,*args, **kwargs)
+        
+class ActorSelectionFrame(Frame):
+    '''
+    Handles the selection by the user of the Meeple or Group which will perform the action
+    '''
+    def __init__(self, *args, **kwargs):
+        Frame.__init__(self,*args, **kwargs)
+        
+class ActionSelectionFrame(Frame):
+    '''
+    Handles the selection by the user of the desired action (costing, or free)
+    '''
+    def __init__(self, *args, **kwargs):
+        Frame.__init__(self,*args, **kwargs)
+        
+class TargetSelectionFrame(Frame):
+    '''
+    Handles the selection by the user of the target of selected action.
+     Can be a Meeple, a Group, a direction (to move), or None(Hunt) 
+    '''
+    def __init__(self, *args, **kwargs):
+        Frame.__init__(self,*args, **kwargs)
+        
+class MeepleStatusFrame(Frame):
+    '''
+    Handles the representation of all the Meeple and their status (remaining AP, Equipment, condition, owner)
+    '''
+    def __init__(self, *args, **kwargs):
+        Frame.__init__(self,*args, **kwargs)
+
+class ConsoleFrame(Frame):
+    '''
+    Handles the consoles output of the GUI.
+    Useful for event, encounters, ...
+    '''
+    def __init__(self, *args, **kwargs):
+        Frame.__init__(self,*args, **kwargs)
--- src/main.py
+++ src/main.py
@@ -3,10 +3,13 @@ Created on 26 avr. 2018
 
 @author: nroux
 '''
+import Tkinter as tk
+
 GAME_VERSION = "0.0"
-from gui import game_gui
+from gui import SoloBetatestGUI
+
 if __name__ == '__main__':
         root = tk.Tk()
-        game_gui(root).pack(side="top", fill="both", expand="true")
+        SoloBetatestGUI(root).pack(side="top", fill="both", expand="true")
         root.title("PHBG Beta-testing tool version "+str(GAME_VERSION))
         root.mainloop()
